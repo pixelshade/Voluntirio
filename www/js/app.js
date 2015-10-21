f97c23a020896670179dfacfe782f495299a39e1
pixelshade <psuldo@gmail.com>
2015-10-21 02:09:40 +0200

    app.js added forgotten

---
+++ www/js/app.js
@@ -0,0 +1,124 @@
+//angular.module('starter', ['ionic', 'ngResource', 'ionicApp.controllers']])
+//
+// .controller('AppCtrl', function() {
+//
+//  ionic.Platform.ready(function() {
+//    navigator.splashscreen.hide();
+//  });
+//
+// })
+
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'ngResource', 'ngOpenFB'])
+
+.run(function ($ionicPlatform, ngFB) {
+    ngFB.init({
+        appId: 'APP_KEY'
+    });
+
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('app', {
+            url: "/app",
+            abstract: true,
+            templateUrl: "templates/menu.html",
+            controller: 'AppCtrl'
+        })
+
+    .state('app.sessions', {
+        url: "/sessions",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/sessions.html",
+                controller: 'SessionsCtrl'
+            }
+        }
+    })
+
+    .state('app.session', {
+        url: "/sessions/:sessionId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/session.html",
+                controller: 'SessionCtrl'
+            }
+        }
+    })
+
+    .state('app.search', {
+        url: "/search",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/search.html"
+            }
+        }
+    })
+
+    .state('app.browse', {
+            url: "/browse",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/browse.html"
+                }
+            }
+        })
+        .state('app.playlists', {
+            url: "/playlists",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/playlists.html",
+                    controller: 'PlaylistsCtrl'
+                }
+            }
+        })
+
+    .state('app.posts', {
+        url: "/posts",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/posts.html",
+                controller: 'PostsCtrl'
+            }
+        }
+    })
+
+    .state('app.post', {
+        url: "/posts/:postId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/post.html",
+                controller: 'PostCtrl'
+            }
+        }
+    })
+
+    .state('app.single', {
+        url: "/playlists/:playlistId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/playlist.html",
+                controller: 'PlaylistCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/sessions');
+});
